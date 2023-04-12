b3ba9bebc99b499dc4a593c6d807608d63adc355
DineshRaju2411 <111958926+DineshRaju2411@users.noreply.github.com>
2023-04-12 17:30:42 +0530

    Update cms.js

--- cms.js
+++ cms.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Prem Kumar')
+  response.send('Hello webHook tag')
 })
 
 app.listen(app.get('port'), function() {
