5e656cfa874a2c8e31126f2f953c5e6fd4b73724
DineshRaju2411 <111958926+DineshRaju2411@users.noreply.github.com>
2023-04-27 12:09:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd /var/www/node/
 git pull https://Dineshraju2411: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@github.com/Dinesh-organization/node.git main
 npm install
 pm2 restart nodeapp
