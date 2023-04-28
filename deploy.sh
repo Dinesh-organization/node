8327b55834ac1bb68fab8fbda93a914959cda1fd
DineshRaju2411 <111958926+DineshRaju2411@users.noreply.github.com>
2023-04-28 14:19:57 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /var/www/node/
-git pull https://Dineshraju2411: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@github.com/Dinesh-organization/node.git main
-npm install
-pm2 restart nodeapp
