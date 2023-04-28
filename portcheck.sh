9e1c9fd1d5366f480f9c860815d6c8e426f9e2e4
DineshRaju2411 <111958926+DineshRaju2411@users.noreply.github.com>
2023-04-28 14:20:15 +0530

    Delete portcheck.sh

--- portcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Check if port 5000 is working
-if nc -zv localhost 5000 > /dev/null 2>&1; then
-  echo "Port 5000 is working"
-else
-  echo "Port 5000 is not working, running revert.sh"
-  /var/www/node/revert.sh
-fi
